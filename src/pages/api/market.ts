fb8eb1d4284ed13eef7f3f65aa6870541bb0ba0e
ngyixuan <ngyixuan2009@gmail.com>
2024-10-05 00:26:18 +0800

    fix: eslint error

--- src/pages/api/market.ts
+++ src/pages/api/market.ts
@@ -1,10 +1,13 @@
-import type { NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import api from ".";
-
 // Method 1: use REST
-export default async function handler(res: NextApiResponse) {
+export default async function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse,
+) {
   try {
     const response = await api.get("/Ticker?pair=BTCUSD,ETHUSD");
+
     res.status(200).json(response.data);
   } catch {
     res.status(500).json({ error: "Error fetching data from Kraken API" });
